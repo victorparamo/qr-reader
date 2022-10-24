6e0e6ca960e6bbb718789ed05f7b4356f4de62b6
victorparamo <victor.h.paramo.24@gmail.com>
2022-10-24 18:24:30 -0500

    dark mode added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
-import styled from '@emotion/styled';
+import Box from '@mui/material/Box';
+import {
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from '@mui/material/styles';
 
 import AppComponent from './components/App';
 import QRValidatorProvider from './Providers/ValidatorProvider';
 
-const StyledApp = styled.div`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-`;
+let theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#3fb5a8',
+    },
+    secondary: {
+      main: '#f5c400',
+    },
+  },
+});
+theme = responsiveFontSizes(theme);
 
 const App = () => (
-  <StyledApp>
-    <QRValidatorProvider>
-      <AppComponent />
-    </QRValidatorProvider>
-  </StyledApp>
+  <ThemeProvider theme={theme}>
+    <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        minHeight: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        bgcolor: 'background.default',
+        color: 'text.primary',
+        borderRadius: 1,
+      }}
+    >
+      <QRValidatorProvider>
+        <AppComponent />
+      </QRValidatorProvider>
+    </Box>
+  </ThemeProvider>
 );
 
 export default App;
