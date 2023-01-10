f701bbed7ca2e3a839350201fd4f2304f913504c
Isra <isralop26@gmail.com>
2023-01-09 20:50:32 -0600

    Moving Event to components directory

--- src/pages/Event/Event.tsx
+++ src/pages/Event/Event.tsx
@@ -5,9 +5,9 @@ import { useNavigate } from 'react-router-dom';
 import getEvent, { EventResponse } from 'api/getEvent';
 import EventHeader from '~/components/EventHeader';
 import FileConfirmationModal from '~/components/FileConfirmationModal';
+import Loading from '~/components/Loading';
 
 import EventTable from './components/EventTable';
-import Loading from './components/Loading';
 
 const Event = (): JSX.Element => {
   const [dataFromCloud, setDataFromCloud] = useState<EventResponse>({
